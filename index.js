63bd2b2589f24b1987c69d537342c04b8b227899
Dominique Loulie <dom.loulie@gmail.com>
2023-05-20 16:27:03 +1000

    this is my first

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const m = document.getElementById('main');
+m.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.textContent = "Dom is the champion";
+
+document.body.append(newHeader);
